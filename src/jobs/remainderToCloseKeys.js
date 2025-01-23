5b118ecb24dc83ef75c58626cd61bc69d0b2e910
Ivan Kolomiyecs <klolo966@gmail.com>
2025-01-23 18:19:25 +0200

    feat: update cron job schedule for key reminders and enhance character data with ratings

--- src/jobs/remainderToCloseKeys.js
+++ src/jobs/remainderToCloseKeys.js
@@ -3,7 +3,7 @@ import { keys } from "../commands/keys.js";
 import { CHANNEL_ID, getChannelByDiscordById } from "../discord/index.js";
 
 export const remainderToCloseKeys = new CronJob(
-  "0 18 * * 4",
+  "0 18 * * 2",
   async () => {
     const channel = await getChannelByDiscordById(CHANNEL_ID);
     const message = await keys();
